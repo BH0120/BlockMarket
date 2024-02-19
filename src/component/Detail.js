d8426c8874021f361f76be26b7c566bcac923184
Jotiger <whdudgh1121@gmail.com>
2024-02-19 17:05:52 +0900

    영호띠

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -10,12 +10,21 @@ function Detail(props) {
 
     const showProduct = () => {
         return (
-            <div>
-                <img src={item.imgUrl} style={{width: "200px"}} alt="" />
-                <h1>{item.name}</h1>
-                <h2>{item.price}</h2>
-                <h2 onClick={() => props.onHeartClick(item.id)}>{item.like ? <GoHeartFill style={{color: 'red'}}/> : <GoHeart/>}</h2>
-                <p>{item.description}</p> {'상세정보창'}
+            <div className='detail'>
+                <div className='detblock'>
+                    <div>
+                        <img src={item.imgUrl} style={{width: "400px"}} alt="" />
+                    </div>
+                    <div>
+                        <h1>{item.name}</h1>
+                        <h2>{item.price}</h2>
+                        <h2 onClick={() => props.onHeartClick(item.id)}>{item.like ? <GoHeartFill style={{color: 'red'}}/> : <GoHeart/>}</h2>
+                        <button className='button'>구매하기</button>
+                    </div>
+                </div>
+                <div className='det-description'>
+                    <p>{item.description}</p>
+                </div>
             </div>
             )
     }
