4b70f0f5364c9140424c1a223f59cc52751077be
BYH0120 <kbh9757@naver.com>
2024-02-21 16:01:36 +0900

    병현 수정

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import Nav from './Nav';
-import Detail from './Detail';
-import './Body.css';
-import LikeBucket from './LikeBucket';
+import React, { useState } from "react";
+import Nav from "./Nav";
+import Detail from "./Detail";
+import LikeBucket from "./LikeBucket";
+import "./Body.css";
 
 function Body(props) {
-    const [selectedItemId, setSelectedItemId] = useState(1);
+  const [selectedItemId, setSelectedItemId] = useState(1);
+//   const [zoomedItemId, setZoomedItemId] = useState(1);
 
-    const handleItemClick = (itemId) => {
-        setSelectedItemId(itemId)
-    };
+  const handleItemClick = (itemId) => {
+    setSelectedItemId(itemId);
+  };
 
-    return (
-        <div className='body'>
-            <Nav products={props.products} onItemClick={handleItemClick}/>
-            {selectedItemId && <Detail products={props.products} onItemClick={selectedItemId} onHeartClick={props.onHeartClick}/>}
-            <LikeBucket products={props.products} onItemClick={handleItemClick}/>
-        </div>
-    );
+//   const handleZoomItemClick = (id) => {
+//     setZoomedItemId(id);
+//     props.onItemClick(id);
+//   };
+
+  return (
+    <div className="body">
+      <Nav products={props.products} onItemClick={handleItemClick} />
+      {selectedItemId && (
+        <Detail
+          products={props.products}
+          onItemClick={selectedItemId}
+          onHeartClick={props.onHeartClick}
+          // onBuy={props.onBuy}
+        />
+      )}
+      <LikeBucket products={props.products} onItemClick={handleItemClick} />
+    </div>
+  );
 }
 
 export default Body;
